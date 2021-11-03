986494030d5c20b84fc64a622bb7cf972f8c4a7b
jacknick7 <josep.ciurana@est.fib.upc.edu>
2021-11-03 13:08:24 +0100

    challenge 2 done

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float dogDelay = 0.5f;
+    private float dogTime = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dogTime > 0)
+        {
+            dogTime -= Time.deltaTime;
+        }
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && dogTime <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            dogTime = dogDelay;
+        }
+    }
+}
