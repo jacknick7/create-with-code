5f8e1ba45c4611482602f9a7ddcef815999915f7
jacknick7 <josep.ciurana@est.fib.upc.edu>
2021-11-20 20:58:58 +0100

    bonus features 3 finished

--- Prototype 3/Assets/Scripts/ScoreManager.cs
+++ Prototype 3/Assets/Scripts/ScoreManager.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     private int score = 0;
-    private PlayerController playerControllerScript; 
+    private PlayerController playerControllerScript;
+
+    public Transform startingPoint;
+    public float lerpSpeed;
     
     // Start is called before the first frame update
     void Start()
     {
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
         InvokeRepeating("IncreaseScore", 0, 1);
+
+        playerControllerScript.gameOver = true;
+        StartCoroutine(PlayIntro());
     }
 
     // Update is called once per frame
@@ -31,5 +37,29 @@ public class ScoreManager : MonoBehaviour
             }
             Debug.Log("Score: " + score + " pts.");
         }
+    }
+
+    IEnumerator PlayIntro()
+    {
+        Vector3 startPos = playerControllerScript.transform.position;
+        Vector3 endPos = startingPoint.position;
+        float journeyLength = Vector3.Distance(startPos, endPos);
+        float startTime = Time.time;
+
+        float distanceCovered = (Time.time - startTime) * lerpSpeed;
+        float fractionOfJourney = distanceCovered / journeyLength;
+
+        playerControllerScript.GetComponent<Animator>().speed = 0.5f;
+
+        while (fractionOfJourney < 1)
+        {
+            distanceCovered = (Time.time - startTime) * lerpSpeed;
+            fractionOfJourney = distanceCovered / journeyLength;
+            playerControllerScript.transform.position = Vector3.Lerp(startPos, endPos, fractionOfJourney);
+            yield return null;
+        }
+
+        playerControllerScript.GetComponent<Animator>().speed = 1.5f;
+        playerControllerScript.gameOver = false;
     }
 }
