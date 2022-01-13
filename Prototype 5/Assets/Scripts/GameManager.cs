e1365ebdd2850f02da3e58cfddf60b5c76a90e9c
jacknick7 <josep.ciurana@est.fib.upc.edu>
2022-01-13 20:39:51 +0100

    implemented game over and restart

--- Prototype 5/Assets/Scripts/GameManager.cs
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,15 +11,21 @@ public class GameManager : MonoBehaviour
 
     private float spawnRate = 1.0f;
 
+    public TextMeshProUGUI gameOverText;
     public TextMeshProUGUI scoreText;
     private int score;
+
+    public bool isGameActive;
+
+    public Button restartButton;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnTarget());
+        isGameActive = true;
         score = 0;
         UpdateScore(0);
+        StartCoroutine(SpawnTarget());
     }
 
     // Update is called once per frame
@@ -28,7 +36,7 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while(true)
+        while(isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
@@ -40,5 +48,17 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
