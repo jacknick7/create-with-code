4c73526eb1d4caf59dd2e1dddf6750c45c9e6645
jacknick7 <josep.ciurana@est.fib.upc.edu>
2022-02-01 19:06:28 +0100

    started replacing car movement for one with forces

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Private variables
-    [SerializeField] float speed = 20.0f;
+    // [SerializeField] float speed = 20.0f;
+    [SerializeField] float horsePower = 0;
     [SerializeField] float turnSpeed = 60.0f;
     private float horizontalInput;
     private float forwardInput;
     public string horizontalAxisName;
     public string verticalAxisName;
+    private Rigidbody playerRb;
+    [SerializeField] GameObject centerOfMass;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.localPosition;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -20,7 +29,8 @@ public class PlayerController : MonoBehaviour
         forwardInput = Input.GetAxis(verticalAxisName);
 
         // Move the vehicle forward based on vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        playerRb.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
         // Turn the vehicle based on horizontal input
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
