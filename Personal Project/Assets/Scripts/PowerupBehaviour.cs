035d54e4a49ad050c2448a395094388b6cdc02b6
jacknick7 <josep.ciurana@est.fib.upc.edu>
2022-02-11 20:31:17 +0100

    added Powerup class + min adjustments

---
+++ Personal Project/Assets/Scripts/PowerupBehaviour.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupBehaviour : MonoBehaviour
+{
+    public const int SHIELD_TO_ADD = 40;
+
+    public float minSpeed = -1.0f;
+    public float maxSpeed = -6.0f;
+
+    public float minTorque = -1.0f;
+    public float maxTorque = -6.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
