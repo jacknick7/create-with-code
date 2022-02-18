1d44a8604ef6241e1e9705e7d74d96e02f1beacd
jacknick7 <josep.ciurana@est.fib.upc.edu>
2022-02-18 20:39:52 +0100

    added version + new powerup script

--- Personal Project/Assets/Scripts/PowerupBehaviour.cs
+++ Personal Project/Assets/Scripts/PowerupBehaviour.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PowerupBehaviour : MonoBehaviour
 {
-    public const int SHIELD_TO_ADD = 40;
+    public const int SHIELD_VALUE = 40;
 
     public float minSpeed = -1.0f;
     public float maxSpeed = -6.0f;
@@ -12,10 +12,14 @@ public class PowerupBehaviour : MonoBehaviour
     public float minTorque = -1.0f;
     public float maxTorque = -6.0f;
 
+    private Rigidbody powerupRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        powerupRb = GetComponent<Rigidbody>();
+        float speed = Random.Range(minSpeed, maxSpeed);
+        powerupRb.velocity = Vector3.left * speed;
     }
 
     // Update is called once per frame
