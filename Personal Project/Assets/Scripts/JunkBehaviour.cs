f40e485633aada92ec0fbdfe311a90df57b96258
jacknick7 <josep.ciurana@est.fib.upc.edu>
2022-02-21 20:51:27 +0100

    finished JunkBehaviour, junk has now the correct direction, speed and torque + hit and score values

--- Personal Project/Assets/Scripts/JunkBehaviour.cs
+++ Personal Project/Assets/Scripts/JunkBehaviour.cs
@@ -4,16 +4,38 @@ using UnityEngine;
 
 public class JunkBehaviour : MonoBehaviour
 {
+    [SerializeField] private int hitValue;
+    [SerializeField] private int scoreValue;
+
+    public float minSpeed = 1.0f;
+    public float maxSpeed = 6.0f;
+
+    public float minTorque = 1.0f;
+    public float maxTorque = 6.0f;
+
+    private float xBound = 16.0f;
+    private float yBound = 7.0f;
+
+    private Rigidbody junkRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        junkRb = GetComponent<Rigidbody>();
+        float speed = Random.Range(minSpeed, maxSpeed);
+        Vector3 dir = new Vector3(-xBound, Random.Range(-yBound, yBound), 0) - transform.position;
+        junkRb.velocity = dir.normalized * speed;
+        float torque = Random.Range(minTorque, maxTorque);
+        junkRb.AddTorque(new Vector3(1, 1, 0) * torque, ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < -xBound || transform.position.x > xBound || transform.position.y < -yBound || transform.position.y > yBound)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
